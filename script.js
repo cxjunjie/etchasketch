749e1183fe13d23d04aa9e58b40b0a2bc1989f16
Junjie <cjunjie09@gmail.com>
2025-11-28 10:22:24 +0800

    Completed adding resize button

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("#resetGrid");
 
-const gridSize = 16;
-const totalSquares = gridSize * gridSize;
-for (let i = 0; i < totalSquares; i++) {
-    const square = document.createElement("div");
-    square.classList.add("grid-square");
+const containerSize = 640;
 
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    });
+function createGrid(gridSize) {
+    // clear existing squares
+    container.innerHTML = "";
 
-    container.appendChild(square);
+    const totalSquares = gridSize * gridSize;
+    const squareSize = containerSize / gridSize;
+
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("grid-square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
+
+        container.appendChild(square);
+    }
 }
+
+createGrid(16);
+
+
+button.addEventListener("click", () => {
+    let input = prompt("Enter number of squares per side (max 100):");
+
+    if (input === null) {
+        alert("Please input a valid positive number.");
+        return;
+    }
+
+    const newSize = parseInt(input, 10);
+
+    if (newSize > 100) {
+        alert("Please input a number less than or equal to 100.");
+        return;
+    }
+
+    createGrid(newSize);
+});
